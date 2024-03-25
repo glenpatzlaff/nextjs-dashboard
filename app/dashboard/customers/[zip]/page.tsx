8c50539e97ab68565927a9fde5ab16c331fd32cc
glenpatzlaff <glen@newwindow.io>
2024-03-25 13:23:42 -0700

    Updated customer page

--- app/dashboard/customers/[zip]/page.tsx
+++ app/dashboard/customers/[zip]/page.tsx
@@ -1,55 +1,78 @@
 async function getData(zip: String) {
-  const apiKey = process.env.GOOGLE_CIVIC_API_KEY; // Access from environment variables
-  const zipCode = zip;
-
-  const res = await fetch(`https://www.googleapis.com/civicinfo/v2/representatives?Levels=country&key=${apiKey}&address=${zipCode}`)
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
- 
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data')
-  }
- 
-  return res.json()
-}
-
-export default async function Page({ params }) {
-  const { zip } = params;
-  const data = await getData(zip)
-  // const jsonString = JSON.stringify(data);
-  return (
-    <div>
-      <h1>Officials Information</h1>
-      <table border="1" style={{ width: '100%', borderCollapse: 'collapse' }}>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Role</th>
-            <th>Party</th>
-            <th>Address</th>
-            <th>Phone</th>
-            <th>Links</th>
-            <th>Social Media</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.officials.map((official, index) => (
-            <tr key={index}>
-              <td>{official.name}</td>
-              <td>{data.offices.find(office => office.officialIndices.includes(index))?.name}</td>
-              <td>{official.party}</td>
-              <td>{official.address?.map(addr => `${addr.line1}, ${addr.city}, ${addr.state} ${addr.zip}`).join(' ')}</td>
-              <td>{official.phones?.join(', ')}</td>
-              <td>{official.urls?.map(url => <a href={url} target="_blank" rel="noopener noreferrer">Link</a>).reduce((prev, curr) => [prev, ', ', curr])}</td>
-              <td>{official.channels?.map(channel => `${channel.type}: ${channel.id}`).join(', ')}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
-};
-
-// export default MyPage;
+    const apiKey = process.env.GOOGLE_CIVIC_API_KEY;
+    const zipCode = zip;
+    const res = await fetch(`https://www.googleapis.com/civicinfo/v2/representatives?Levels=country&key=${apiKey}&address=${zipCode}`);
   
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+  
+    return res.json();
+  }
+  
+  export default async function Page({ params }) {
+    const { zip } = params;
+    const data = await getData(zip);
+  
+    return (
+      <div>
+        <h1>Officials Information</h1>
+        <table border="1" style={{ width: '100%', borderCollapse: 'collapse' }}>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Role</th>
+              <th>Party</th>
+              <th>Address</th>
+              <th>Phone</th>
+              <th>Links</th>
+              <th>Social Media</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.officials.map((official, index) => (
+              <tr key={index}>
+                <td>{official.name}</td>
+                <td>{data.offices.find(office => office.officialIndices.includes(index))?.name}</td>
+                <td>{official.party}</td>
+                <td>
+                  {official.address && (
+                    <ul>
+                      {official.address.map((addr, addrIndex) => (
+                        <li key={addrIndex}>
+                          {`${addr.line1}, ${addr.city}, ${addr.state} ${addr.zip}`}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </td>
+                <td>{official.phones?.join(', ')}</td>
+                <td>
+                  {official.urls && (
+                    <ul>
+                      {official.urls.map((url, urlIndex) => (
+                        <li key={urlIndex}>
+                          <a href={url} target="_blank" rel="noopener noreferrer">Link</a>
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </td>
+                <td>
+                  {official.channels && (
+                    <ul>
+                      {official.channels.map((channel, channelIndex) => (
+                        <li key={channelIndex}>
+                          {`${channel.type}: ${channel.id}`}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
